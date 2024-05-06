a144181bd9122bd7dfe7e94352511b176a7bb017
tracer-ics2017 <tracer@njuics.org>
2024-05-06 17:10:39 +0800

    >  run 2111454 sesame Linux sesame-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  17:10:39 up  1:27,  1 user,  load average: 0.00, 0.00, 0.00 4d2fca3cb90f4722f36978e8df878f286c6284c

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -3,14 +3,12 @@
 
 int sys_write(int fd, void *buf, size_t len) {
     if(fd == 1 || fd == 2){
-        char c;
+        char* c = (char*) buf;
         for(int i = 0; i < len; i++) {
-            memcpy(&c ,buf + i, 1);
-            _putc(c);
+            _putc(c[i]);
         }
         return len;
     }
-    Log("fd <= 0");
     return -1;
 }
 
@@ -22,10 +20,10 @@ _RegSet* do_syscall(_RegSet *r) {
   a[3] = SYSCALL_ARG4(r);
 
   switch (a[0]) {
-    case SYS_none:  SYSCALL_ARG1(r) = 1; break;
+    case SYS_none:  r->eax = 1; break;
     case SYS_exit:  _halt(a[1]); break;
-    case SYS_write: SYSCALL_ARG1(r) = sys_write(a[1], (void *)a[2], a[3]); break;
-    case SYS_brk:   SYSCALL_ARG1(r) = 0; break;
+    case SYS_write: r->eax = sys_write(a[1], (void *)a[2], a[3]); break;
+    //case SYS_brk:   r->eax = 0; break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 
