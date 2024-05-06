7457e70cb48da6ee9bb803cf10e3f18ee17cae9d
tracer-ics2017 <tracer@njuics.org>
2024-05-06 15:48:40 +0800

    >  compile 2111454 sesame Linux sesame-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  15:48:40 up 5 min,  1 user,  load average: 0.11, 0.16, 0.09 80d158344bcda824c2c35deb55f138a0f7d33da0

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -2,7 +2,12 @@
 
 #define DEFAULT_ENTRY ((void *)0x4000000)
 
+extern void ramdisk_read(void *buf, off_t offset, size_t len);
+extern size_t get_ramdisk_size();
+
 uintptr_t loader(_Protect *as, const char *filename) {
-  TODO();
+  // TODO();
+  size_t len = get_ramdisk_size();
+  ramdisk_read(DEFAULT_ENTRY,0,len);
   return (uintptr_t)DEFAULT_ENTRY;
 }
