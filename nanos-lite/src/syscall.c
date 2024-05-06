d51bd221a1b5f943bee1d4a9a8fa2db55632dc0d
tracer-ics2017 <tracer@njuics.org>
2024-05-06 16:33:07 +0800

    >  run 2111454 sesame Linux sesame-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  16:33:07 up 49 min,  2 users,  load average: 0.00, 0.00, 0.00 7c1f9b6962739d28b02af6fb889f29f854d9e9b0

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,6 +1,17 @@
 #include "common.h"
 #include "syscall.h"
 
+uintptr_t sys_write(int fd, const void *buf, size_t len) {
+  uintptr_t i = 0;
+  if(fd == 1 || fd == 2){
+    for(; len > 0; len--) {
+      _putc(((char*)buf)[i]);
+      i++;
+    }
+  }
+  return i;
+}
+
 _RegSet* do_syscall(_RegSet *r) {
   uintptr_t a[4];
   a[0] = SYSCALL_ARG1(r);
@@ -11,6 +22,7 @@ _RegSet* do_syscall(_RegSet *r) {
   switch (a[0]) {
     case SYS_none:  r->eax = 1; break;
     case SYS_exit:  _halt(a[1]); break;
+    case SYS_write: r->eax = sys_write(a[1], (void *)a[2], a[3]); break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 
