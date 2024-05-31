ac40f27952f0ac8aa262da749a99235472ee411e
tracer-ics2017 <tracer@njuics.org>
2024-06-01 06:41:27 +0800

    >  run 2111454 sesame Linux sesame-virtual-machine 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  06:41:27 up  1:25,  1 user,  load average: 0.06, 0.03, 0.01 64fcb6c2fa1e683fcf4781d9625d48abd0c5b85

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -1,5 +1,6 @@
 #include "common.h"
 #include "fs.h"
+#include "memory.h"
 
 #define DEFAULT_ENTRY ((void *) 0x8048000)
 
@@ -13,12 +14,24 @@ extern size_t get_ramdisk_size();
 
 uintptr_t loader(_Protect *as, const char *filename) {
   // TODO();
-//   size_t len = get_ramdisk_size();
-//   ramdisk_read(DEFAULT_ENTRY,0,len);
-//   return (uintptr_t)DEFAULT_ENTRY;
-    int fd = fs_open(filename, 0, 0);
-    Log("filename=%s,fd=%d",filename,fd);
-    fs_read(fd, DEFAULT_ENTRY, fs_filesz(fd));
-    fs_close(fd);
-    return (uintptr_t)DEFAULT_ENTRY;
+  // ramdisk_read(DEFAULT_ENTRY, 0, RAMDISK_SIZE);
+  int fd = fs_open(filename, 0, 0);
+  Log("filename=%s,fd=%d",filename,fd);
+  // fs_read(fd, DEFAULT_ENTRY, fs_filesz(fd));
+  int size = fs_filesz(fd);
+  int ppnum = size / PGSIZE;
+  if(size % PGSIZE != 0) {
+    ppnum++;
+  }
+  void *pa = NULL;
+  void *va = DEFAULT_ENTRY;
+  for(int i = 0; i < ppnum; i++) {
+    pa = new_page();
+    _map(as, va, pa);
+    fs_read(fd, pa, PGSIZE);
+    va += PGSIZE;
+  }
+
+  fs_close(fd);
+  return (uintptr_t)DEFAULT_ENTRY;
 }
